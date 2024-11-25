3ae749eafabc6d59801a24d453b5518a216d282c
Khegal <khegal95@gmail.com>
2024-11-25 18:42:01 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { SignIn } from "@/components/SignIn";
+
+const Home = () => {
+  return (
+    <div className=" flex justify-center items-center h-screen flex-grow">
+      <div className="mt-8 mx-auto pb-8 max-w-md">
+        <div className="mt-3">
+          <SignIn></SignIn>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ components/SignIn.tsx
@@ -0,0 +1,13 @@
+export const SignIn = () => {
+  return (
+    <div className="border py-2.5 flex justify-center w-[350px] flex-grow">
+      <div className="h-[55px] mt-9 mb-3">
+        <div
+          role="img"
+          aria-label="Instagram"
+          className="bg-[url('https://static.cdninstagram.com/rsrc.php/v3/ym/r/BQdTmxpRI6f.png')] bg-no-repeat bg-[0px_0px] bg-[length:176px_181px] w-[175px] h-[51px]"
+        ></div>
+      </div>
+    </div>
+  );
+};
